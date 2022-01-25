54312fa7b488f9a1437659a64b0524bdf3d128ae
Jake Jackson <jakecjackson99@gmail.com>
2022-01-25 17:15:02 +0000

    Update main.py TODO list

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 TODO
 1) Read each file
 2) For each file, split the text up into sentences (i.e. .split("."))
-    2.1) Before reading the split text, remove the punctuation and lower the word's case
+    2.1) Before reading the split text, remove the punctuation and lower the text's case
 3) Identify "interesting" words (decide what that means later)
-4) If the word is in the dictionary, increment the key's "counter" value; else add it to the dict
+4) If the word is in the dictionary, increment the key's "counter" value; else, add it to the dict
    along with the file name it occurs in and the sentence itself WITH the punction.
 
 NOTE
