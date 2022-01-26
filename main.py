58860f8ae6d694b175a70f94ffe6c99dff7aa495
JakeJackson99 <jakecjackson99@gmail.com>
2022-01-26 21:17:44 +0000

    Implement algorithm

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ TODO
 
 NOTE
 - Only add a word's location if it hasn't already been added (cannot use sets in dictionaries)
+*- Stripping sentences of their punctuation also strips any necessary apostrophes in words
+    - Maybe keep track of the word's index in the sentence BEFORE stripping away the punctuatio
+      so it can be added into the 'words' dictionary
+- For printing the values, if not being done in the browser, create a table following this guide:
+  https://blog.softhints.com/python-print-pretty-table-list/
 
 Format:
 words = {
@@ -19,4 +24,47 @@ words = {
     }
 }
 """
+import string
 
+
+def sentence_cleaner(text):
+    for char in string.punctuation:
+        text = text.replace(char, "")
+    return text.lower().split()
+
+def main():
+    results = {}
+
+    for i in range(1, 7):
+        with open(f"test_docs/doc{i}.txt") as file:
+            text = file.read().split(".")
+            for sentence in text:
+                if sentence == "":
+                    continue
+                sentence = sentence.strip()
+                words = sentence_cleaner(sentence)
+                for word in words:
+                    if word not in results:
+                        results.update({
+                            word: {"counter": 0,
+                                   "documents": [], "sentences": []}
+                        })
+
+                    results[word]["counter"] += 1
+                    if f"doc{i}.txt" not in results[word]["documents"]:
+                        results[word]["documents"].append(f"doc{i}.txt")
+                    if sentence not in results[word]["sentences"]:
+                        results[word]["sentences"].append(sentence)
+
+    for key, value in results.items():
+        if 5 < value["counter"] < 8:
+            print(key, ' -> ', value)
+
+
+if __name__ == "__main__":
+    main()
+
+    # TESTS
+    # for key, value in results.items():
+    #     if value["counter"] > 5:
+    #         print(key, ' -> ', value)
